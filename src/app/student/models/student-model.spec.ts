26628f714424c556856b568305bd4c35d21c81f5
redg <regis.deroo@hotmail.com>
2023-03-14 10:49:46 +0100

    get/set on Student model attribute

--- src/app/student/models/student-model.spec.ts
+++ src/app/student/models/student-model.spec.ts
@@ -4,4 +4,16 @@ describe('StudentModel', () => {
   it('should create an instance', () => {
     expect(new StudentModel()).toBeTruthy();
   });
+
+
+  it (`should have 'Aubert as lastName`, () =>{
+    const student : StudentModel = new StudentModel()
+    //fill student object
+    student.lastName = 'Aubert'
+    student.email = 'jean-luc.aubert@aelion.fr'
+    student.login = 'jl'
+    student.password ='123456'
+
+    expect(student.lastName).toBe('Aubert')
+  })
 });
--- src/app/student/models/student-model.ts
+++ src/app/student/models/student-model.ts
@@ -1,2 +1,72 @@
 export class StudentModel {
+  private _id?: number
+  private _lastName: string = ''
+  private _firstName?: string = ''
+  private _email: string = ''
+  private _phoneNumber: string = ''
+  private _login: string = ''
+  private _password: string = ''
+
+
+  /**
+   * @usage
+   * const student: StudentModel = new StudentModel()
+   * student.id = 10
+   * console.log(student.id)// magis getter
+   */
+  get id() {
+    return this._id === undefined ? 0 : this._id
+  }
+
+  set id(val: number) {
+    this._id = val
+  }
+
+  get lastName() {
+    return this._lastName
+  }
+
+  set lastName(val: string) {
+    this._lastName = val
+  }
+
+  get firstName() {
+    return this._firstName === undefined ? '' : this._firstName
+  }
+
+  set firstName(val: string) {
+    this._firstName = val
+  }
+
+  get email() {
+    return this._email
+  }
+
+  set email(val: string) {
+    this._email = val
+  }
+
+  get phoneNumber() {
+    return this._phoneNumber
+  }
+
+  set phoneNumber(val: string) {
+    this._phoneNumber = val
+  }
+
+  get login() {
+    return this._login
+  }
+
+  set login(val: string) {
+    this._login = val
+  }
+
+  get password() {
+    return this._password
+  }
+
+  set password(val: string) {
+    this._password = val
+  }
 }
